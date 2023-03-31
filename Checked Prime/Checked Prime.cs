ed005cd7357149d431f5c6bce952ed3595b3d1e9
KaisBos <94121595+KaisBos@users.noreply.github.com>
2023-03-31 13:35:27 +0700

    Checked Prime

---
+++ Checked Prime/Checked Prime.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+
+namespace Checked_Prime
+{
+    class Checked_Prime
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Enter your number: ");
+                int number = Int32.Parse(Console.ReadLine());
+
+            int check = 1;
+            bool prime = true;
+
+
+            while (check < Math.Sqrt(number))
+            {
+                check++;
+                if (number % check == 0)
+                {
+                    prime = false;
+                    break;
+                }                
+            }
+
+            if (number == 2 | (prime && number != 1 && number > 2))
+            {
+                Console.WriteLine("{0} is a prime", number);
+            }
+            else
+            {
+            Console.Write("{0} isn't a prime", number);
+            }
+        }   
+    }
+}
