92eba200b21122e3a8a3435f308081600e9d3a6e
KaisBos <94121595+KaisBos@users.noreply.github.com>
2023-03-31 08:31:46 +0700

    Linear Equation Resolver

---
+++ Linear Equation Resolver/Linear Equation Resolver.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace LinearEquationResolver
+{
+    class LinearEquationResolver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0, enter constants:");
+
+            Console.Write("a = "); double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = "); double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a != 0)
+            {
+                Console.Write("The solution is " + (-b / a));
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("The solution is all x!");
+                }
+                else
+                {
+                    Console.Write("There is no solution!");
+                }
+            }            
+        }
+    }
+}
