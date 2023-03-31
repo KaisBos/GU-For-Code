bc5e5fc04b7ed7fe1c5fdc3b0afb014ac9392b97
KaisBos <94121595+KaisBos@users.noreply.github.com>
2023-03-31 09:48:20 +0700

    Body Mass Index-BMI

---
+++ Body Mass Index-BMI/Body Mass Index-BMI.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace LinearEquationResolver
+{
+    class LinearEquationResolver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Body mass index-BMI");
+
+            Console.Write("Your weight (kg): ");
+                double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Your height (m): ");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+            double BMI = weight / Math.Pow(height,2);
+            BMI = Math.Round(BMI,1);
+
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("You are Underweight!");
+            }
+            else if (BMI < 25.0)
+            {
+                Console.WriteLine("You are Normal!");
+            }
+            else if (BMI < 30.0)
+            {
+                Console.WriteLine("You are Overweight!");
+            }
+            else
+            {
+                Console.WriteLine("You are Obese!");
+            }
+        }
+    }
+}
